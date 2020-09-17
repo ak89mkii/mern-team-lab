65218389538a914fb5630dd2cdd31591d490e9ab
pratik <engrpatel21@gmail.com>
2020-09-17 10:09:11 -0400

    update user model

--- models/user.js
+++ models/user.js
@@ -6,7 +6,10 @@ const SALT_ROUNDS = 6;
 const userSchema = new mongoose.Schema({
   name: String,
   email: {type: String, required: true, lowercase: true, unique: true},
-  password: String
+  password: String,
+  bio: String,
+  avatar: {type: String, default: 'https://picsum.photos/200/300'},
+  favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Anime'}]
 }, {
   timestamps: true
 });
